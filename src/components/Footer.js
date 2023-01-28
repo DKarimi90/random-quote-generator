05bef6446765373c1a401facdce967146d584996
david <davmwa90@gmail.com>
2023-01-28 19:31:19 +0300

    updated app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='footer'>
         <h3>&copy; Copyright 2023</h3><hr />
-        <h3>Site by SoftGeek</h3><br />
+        <h3>App by SoftGeek</h3><br />
     
     </div>
   )
