0901895d1c9e42ed4151d5cb8461155e970215ba
david <davmwa90@gmail.com>
2023-01-28 23:41:04 +0300

    Updated css

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
+    <div className='container'>
         {/* <h1>Click on Quotes to Generate Some random Quotes!</h1> */}
         <p>Never be controlled by your past, people and money</p>
-        <h2>*Anonymous*</h2>
-        <p>Self-discipline means living the hard-way; basically resisting temptations and immediate gratification in order to receive bigger and better rewards future</p>
-        <h2>*Martin Meadows*</h2>
+        <h4>Anonymous</h4>
+        <p>Self-discipline means living the hard-way; basically resisting temptations and immediate gratification in order to receive bigger and better rewards in future</p>
+        <h4>Martin Meadows</h4>
         <p>Never quit believing for not seeing; always stay in faith and trust in God</p>
-        <h2>*Joel Osteen*</h2>< hr/>
+        <h4>Joel Osteen</h4>< hr/>
 
         <h3>Tap on Quotes to Generate more uplifting Quotes</h3>
 
