194acbd484b567162a8622dc9f075af19c9d3cea
david <davmwa90@gmail.com>
2023-01-29 00:04:13 +0300

    updated the app

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,9 @@ import React from 'react'
 function About() {
   return (
     <div>
-      A simple application where you can randomly generate quotes by various famous persons from around the world 
+      <p>
+        A simple application where you can randomly generate quotes by various famous persons from around the world 
+        </p>
     </div>
   )
 }
